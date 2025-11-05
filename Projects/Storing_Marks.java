6a6ee438ea72f1849eb4dd9a4a030c4781895321
SAHALAM <99486794+SAHALALAM88@users.noreply.github.com>
2025-11-05 11:05:53 +0530

    Enhance Storing_Marks.java with comments
    Added comments to improve code readability.

--- Projects/Storing_Marks.java
+++ Projects/Storing_Marks.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Train {
     public static void main(String[] args) {
-        
+
+        //Initializing Scanner Class To Take Input From The User 
         Scanner sc = new Scanner(System.in);
+
+        //Taking Input From User : 
 
         System.out.println("Enter The Number Of Students : ");
         int n = sc.nextInt();
@@ -11,10 +14,12 @@ public class Train {
         System.out.println("Enter The Number Of Subjects : ");
         int sub = sc.nextInt();
 
+        //Declaring Arrays To Store Student Names, Roll Numbers, And Marks
         String[] names = new String[n];
         int[] RollNo = new int[n];
         int[][] marks = new int[n][sub];
 
+        //Taking Input Of Each Student's Name And Roll Number
         for(int i=0;i<n;i++){
             System.out.println("Enter The Name Of Student " + (i+1) + " : ");
             names[i] = sc.next();
@@ -23,6 +28,7 @@ public class Train {
             RollNo[i] = sc.nextInt();
         }
 
+        //Taking Input Of Marks For Each Student In All Subjects
         for (int i=0;i<n;i++) {
             System.out.println("Enter The Marks Of Student " + (i+1) + " : ");
             for(int j=0;j<sub;j++){
@@ -30,6 +36,8 @@ public class Train {
                 marks[i][j] = sc.nextInt();
             }    
         }
+
+        //Displaying The Marks Of Each Student For Every Subject
         for(int i=0;i<n;i++){
             for(int j=0;j<sub;j++){
                 System.out.println("The Marks Of Student " + (i+1) + " For Subject " + (j+1) + " : " + (marks[i][j]));
